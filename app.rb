c20687a26b45c8b98ca07a0e07e10ae1fb420bb7
Katsuyoshi Ito <kito@itosoft.com>
2022-11-15 23:07:45 +0900

    Bootstrapで体裁を整える

--- app.rb
+++ app.rb
@@ -7,18 +7,19 @@ require './scripts/daisen_open_data'
 require 'sinatra_more/markup_plugin'
 
 Sinatra::Base.register SinatraMore::MarkupPlugin
+set :haml, { escape_html: false }
 
-before '/|/graph' do
+before '/|/data_table' do
   @daisen_data = DaisenOpenData.instance.data
 end
 
 get '/' do
-  haml :index, :escape_html => false
+  haml :index, :layout => :layout
 end 
 
-get '/graph/:kind' do
+get '/data_table/:kind' do
   @daisen_data = DaisenOpenData.instance.data
 #p @daisen_data
   @kind = params[:kind]
-  haml :graph 
+  haml :data_table, :layout => :layout
 end
